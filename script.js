311fd6023b2521bbdaa5b733cac16de5dd770d95
Nicola Salsotto <nicolasalsotto@gmail.com>
2018-05-28 19:40:05 +0200

    Add basic bootstrap theming

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var flipped = 0;
 var c1 = "";
 var c2 = "";
 var moves = 0;
+var difficulty = "medium"
+var interval;
 
 function shuffle(a) {
   var j, x, i;
@@ -23,6 +25,9 @@ function checkMatch() { //To be called when 2 cards are uncovered
     	$(".card").filter(function() {
           return $($(this).children()[1]).css('background-color') == c1;
       }).hide();
+      $(".card").filter(function() {
+          return $($(this).children()[1]).css('background-color') == c1;
+      }).remove();
     });
   } else { //Not equal, unflip them
     $(".card").flip(false);
@@ -36,6 +41,9 @@ function checkMatch() { //To be called when 2 cards are uncovered
 
 function initializeWithSize(r, c) {
   console.log("r = " + r + " - c = " + c);
+  clearInterval(interval);
+  moves = 0;
+  $("#moves").html("Moves: " + moves);
 
   var i, j;
   var colors8 = ["#FF0000", "#FF0000", "#FF7F00", "#FF7F00",
@@ -73,7 +81,7 @@ function initializeWithSize(r, c) {
   }
   content += "</table>"
 
-  $('#memTab').append(content);
+  $('#memTab').replaceWith(content);
   $(".card").flip({
     trigger: "manual"
   });
@@ -95,6 +103,25 @@ function initializeWithSize(r, c) {
     if (flipped == 2)
       setTimeout(checkMatch, 800);
   });
+
+  interval = setInterval(checkWinner, 100);
 }
 
-initializeWithSize(4, 6);
+$("button").click(function () {
+	$("button").removeClass('btn-primary').addClass('btn-default');
+	$(this).removeClass('btn-default').addClass('btn-primary');
+  difficulty = $(this).data("level");
+  if (difficulty == "easy") initializeWithSize(2, 4);
+  if (difficulty == "medium") initializeWithSize(4, 4);
+  if (difficulty == "hard") initializeWithSize(4, 6);
+})
+
+function checkWinner() {
+  if ($(".card").length == 0) {
+    console.log("Pazzesco");
+    $('#memTab').replaceWith("<p>You win!</p>");
+    clearInterval(interval);
+  }
+}
+
+//initializeWithSize(4, 4);
